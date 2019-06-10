dca7efb55eba0889863f690e9e409ced6071e56b
huyg <742315209@qq.com>
2019-06-10 13:45:38 +0800

    update

--- lib.frame/src/main/java/com/xlgcx/frame/view/ToolbarActivity.java
+++ lib.frame/src/main/java/com/xlgcx/frame/view/ToolbarActivity.java
@@ -69,6 +69,7 @@ public abstract class ToolbarActivity extends RxAppCompatActivity {
                     backAction();
                 }
             });
+
             setActionBar();
         }
     }
